45dba926aef62a9ff182e5879e072190ff883451
ImRaffy14 <raffysolis123@gmail.com>
2024-04-02 13:57:29 +0800

    added: projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -11,12 +11,28 @@ function Projects ({isLight}){
           <div className="max-w-screen-xl mx-auto">
             <div className="flex flex-wrap my-[100px] space-x-0 sm:space-x-5 justify-center">
 
-                <div className="card w-[280px] h-[280px] lg:w-[380px] lg:h-[280px] bg-neutral-800 hover:scale-105 duration-500 cursor-pointer my-4 ml-0 sm:ml-4">
+                <div className="card w-[280px] h-[280px] lg:w-[380px] lg:h-[280px] bg-neutral-800 hover:scale-105 duration-500 cursor-pointer my-4 ml-0 sm:ml-4" onClick={()=>document.getElementById('my_modal_5').showModal()}>
                     <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
                     <div className="card-body">
                         <h2 className="text-center font-bold text-lg text-neutral-200">Life hack</h2>
                     </div>
                 </div>
+
+                <dialog id="my_modal_5" className="modal card-bottom sm:card-middle">
+                <div className="card lg:card-side bg-gray-800 shadow-xl">
+                <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+                <div className="card-body">
+                <form method="dialog">
+                    <button className="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
+                </form>
+                    <h2 className="card-title">New album is released!</h2>
+                    <p>Click the button to listen on Spotiwhy app.</p>
+                    <div className="card-actions justify-end">
+                    </div>
+                </div>
+                </div>
+          
+                </dialog>
 
                 <div className="card w-[280px] h-[280px] lg:w-[380px] lg:h-[280px] bg-neutral-800 hover:scale-105 duration-500 cursor-pointer my-4">
                     <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!"/></figure>
